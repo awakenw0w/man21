b7ce97de831afd2beb975f3cca0284d840e24d9c
Your Name <126647399+H3LLG0@users.noreply.github.com>
2024-11-14 11:48:26 +0400

    Initial commit

---
+++ src/Man.java
@@ -0,0 +1,13 @@
+public class Man {
+    // конструктор класса (данная функция вызывается
+    // при создании нового экземпляра класса
+    public Man(String _name) {
+        this.Name = _name;
+        isLife = true;
+    }
+
+        // поля класса (атрибуты объекта)
+    String Name;
+    Boolean isLife;
+
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int x = 4;
+        Man newman= new Man(_name: "егор");
+
+
+    }
+}
